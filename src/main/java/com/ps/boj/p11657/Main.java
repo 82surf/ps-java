7e297a2c93fe9efbf006f92b53969d66c62daa13
Hyeon Soo Yoo <82surf@gmail.com>
2025-01-17 23:59:47 +0900

    update: boj 11657

--- src/main/java/com/ps/boj/p11657/Main.java
+++ src/main/java/com/ps/boj/p11657/Main.java
@@ -34,12 +34,17 @@ public class Main {
     }
 
     public static void bellmanFord(int start, int N, int M, List<Edge> graph) {
+        // 출발 노드에서 다른 노드까지의 최단거리를 표현하는 배열
         long[] dist = new long[N + 1];
         long INF = Long.MAX_VALUE;
         Arrays.fill(dist, INF);
         dist[start] = 0;
 
+        // 정점의 개수 - 1번 완화 알고리즘을 수행한다.
+        // 한 정점에서 다른 정점으로 이동할 때 지날 수 있는 간선의 최대 개수가 N - 1이기 때문.
         for (int i = 0; i < N - 1; i++) {
+            // 완화(Relax) 알고리즘
+            // 간선을 순회하며 최단거리를 갱신할 수 있다면 갱신한다.
             for (Edge edge : graph) {
                 if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
                     dist[edge.to] = dist[edge.from] + edge.cost;
@@ -47,6 +52,8 @@ public class Main {
             }
         }
 
+        // 위 과정을 거친 이후에도 최단거리가 감소하는 경우, 음의 사이클이 존재한다고 볼 수 있다.
+        // 가능한 짧은 거리로 갱신했음에도 최단거리가 줄어든다는 것은 최단거리가 음의 무한대인 음의 사이클이 존재한다는 뜻.
         for (Edge edge : graph) {
             if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
                 System.out.println(-1);
