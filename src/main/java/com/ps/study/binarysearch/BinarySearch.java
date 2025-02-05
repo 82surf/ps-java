7eef9cf244014149fd60255acd8d69518bec3931
Hyeon Soo Yoo <82surf@gmail.com>
2025-02-05 21:19:08 +0900

    update: binary search example

--- src/main/java/com/ps/study/binarysearch/BinarySearch.java
+++ src/main/java/com/ps/study/binarysearch/BinarySearch.java
@@ -4,44 +4,58 @@ public class BinarySearch {
     
     public static void main(String[] args) {
         // 예시용 배열(미리 정렬)
-        int[] arr = {1, 3, 5, 5, 5, 7, 9, 9, 9, 11, 13, 15};
+        int[] arr = {1, 3, 5, 5, 5, 7, 9};
 
-        // lowerBound
-        int lo = lowerBound(arr, 5);
-        System.out.println("lo: " + lo + " val: " + arr[lo]);
+        // 5 이상인 수가 처음 나오는 인덱스
+        int loIdx = lowerBound(arr, 5);
+        System.out.println("loIdx: " + loIdx + " val: " + arr[loIdx]);
 
-        // upperBound
-        int up = upperBound(arr, 5);
-        System.out.println("up: " + up + " val: " + arr[up]);
+        // 5 초과인 수가 처음 나오는 인덱스
+        int upIdx = upperBound(arr, 5);
+        System.out.println("upIdx: " + upIdx + " val: " + arr[upIdx]);
     }
 
+    /**
+     * @param arr
+     * @param target
+     * @return target 이상의 수가 처음 나오는 인덱스
+     */
     static int lowerBound(int[] arr, int target) {
+        // 구간은 [left, right)로 설정한다.
         int left = 0;
         int right = arr.length;
 
-        while (left + 1 < right) {
+        while (left < right) {
             int mid = (left + right) / 2;
             if (arr[mid] < target) {
-                left = mid;
+                left = mid + 1;
             } else {
                 right = mid;
             }
         }
+        // while문이 종료되면 left와 right는 동일해지므로 무엇을 반환해도 상관 없다.
         return left;
     }
 
+    /**
+     * @param arr
+     * @param target
+     * @return target을 초과하는 수가 처음 나오는 인덱스
+     */
     static int upperBound(int[] arr, int target) {
+        // 구간은 [left, right)로 설정한다.
         int left = 0;
         int right = arr.length;
 
         while (left + 1 < right) {
             int mid = (left + right) / 2;
             if (arr[mid] <= target) {
-                left = mid;
+                left = mid + 1;
             } else {
                 right = mid;
             }
         }
+        // while문이 종료되면 left와 right는 동일해지므로 무엇을 반환해도 상관 없다.
         return right;
     }
 }
